9d6990f99e8ed6e0cd149952585cc79ab8ce5ad9
GiliSasportas <123905147+GiliSasportas@users.noreply.github.com>
2024-03-27 23:55:27 +0000

    update

---
+++ test/store/wallet/fuzztestGabaim.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/wallet/Gabaim.sol";
+
+
+contract fuzztestGabaim is Test {
+
+     Gabaim public wallet;
+
+     function setUp() public {
+        wallet = new Gabaim();
+    }
+
+     function testDeposite(uint sum) public {
+        uint initialBalance = wallet.getBalance();
+        address ad=vm.addr(20);
+        vm.startPrank(ad);
+        vm.deal(ad,sum);
+        payable(wallet).transfer(sum);
+        assertEq(wallet.getBalance(), initialBalance + sum, "Balance should increase by num after transfer");
+        vm.stopPrank();
+    }
+    function fuzztestWithdraw(uint sum) payable public {
+      payable(wallet).transfer(sum);
+      uint balances = wallet.getBalance();
+      address ad = vm.addr(12);
+      wallet.addGabay(ad);
+      vm.prank(ad);
+      wallet.withdraw(sum);
+      assertEq(wallet.getBalance(),balances-sum);
+    }
+
+    function testaddGabay(address ad) public{
+      wallet.addGabay(address(ad));
+      assertEq(wallet.countowners(),1);
+      assertEq(wallet.owners(address(ad)),true);
+    }
+
+     function testaddGabayAlreadyExist(address ad) public{
+      wallet.addGabay(address(ad));
+      assertEq(wallet.countowners(),1);
+      assertEq(wallet.owners(address(ad)),true);
+      vm.expectRevert("the gabay already exist");
+      wallet.addGabay(address(ad));
+    }
+
+
+
+   function testNotOwnerAddGabay(address ad) public{
+      address d= vm.addr(12);
+       vm.startPrank(d);
+       vm.expectRevert("you not owner");
+       wallet.addGabay(address(ad));
+       vm.stopPrank();
+   }
+
+    function testHaveTreeGabaim(address ad) public{
+        wallet.addGabay(vm.addr(1));
+        wallet.addGabay(vm.addr(2));
+        wallet.addGabay(vm.addr(3));
+        assertEq(wallet.countowners(),3);
+        vm.expectRevert("There are already 3 owners");
+        wallet.addGabay(address(ad));
+    }
+
+    function testChangeGabay(address adold, address adnew) public{
+      wallet.addGabay(address(adold));
+      wallet.changeGabay(adold,adnew);
+      assertEq(wallet.owners(address(adold)),false);
+      assertEq(wallet.owners(address(adnew)),true);
+    }
+
+}

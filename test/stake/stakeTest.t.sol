bf875449958e758baa9a0ccc724ccc5a9c5cace8
gili <665gili@gmail.com>
2024-04-11 00:54:38 +0300

    update

--- test/stake/stakeTest.t.sol
+++ test/stake/stakeTest.t.sol
@@ -13,8 +13,8 @@ contract StakeTest is Test {
 
 
     function setUp() public {
-        t= new myToken();
-        s = new Stake(address(t));
+        t= new myToken();//IERC20
+        s = new Stake(address(t));//STAKE
     }
 
     function testMint() public {
@@ -23,18 +23,32 @@ contract StakeTest is Test {
      }
 
     function testStake() public{
+        console.log("log",address(this),address(s));
         t.mint(address(this),100); 
-        t.approve(address(s), 50);
+        t.approve(address(s), 50);//מסכים ל-stake למשוך 50
         s.stake(50);
         assertEq(t.balanceOf(address(this)), 50);
     }
 
+    function testwithdraw() public{
+        t.mint(address(this),100); 
+        t.approve(address(s), 50);//מסכים ל-stake למשוך 50
+        s.stake(50);
+        //t.transfer(s.staking[msg.sender].sum,100);
+       // uint256 b=s.staking[msg.sender].sum;
+        uint256 b= t.balanceOf(address(this));
+        console.log(b,"bbbbbb");
+        assertEq(t.balanceOf(address(this)), 50);
+        s.whithdraw(50);
+        uint256 c= t.balanceOf(address(this));
 
-    // function stake(uint amount) public{
-    //         staking[msg.sender].sum+=amount;
-    //         staking[msg.sender].date=block.timestamp;
-    //         i.transferFrom(msg.sender,address(this), amount);
-    //     }
+        // uint256 bafter=s.staking[msg.sender].sum;
+        // console.log(b,"b",bafter,"bafter");
+        assertEq(t.balanceOf(address(this)), b-50);
+    }
+
+
+    
 
 
 }

1e6bf1a54f4f233bc41bf619c345311b645c21d8
Gili <665gili@gmail.com>
2024-05-19 16:59:53 +0300

    update

--- src/lending/lending.sol
+++ src/lending/lending.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "@hack/auction/myERC721.sol";
+import "lib/forge-std/src/interfaces/IERC20.sol";
 
 contract Lending{
 
@@ -13,36 +14,36 @@ contract Lending{
     IERC20 public  tokenDAI;
     IERC20 public tokenLending;
     IERC20 public tokenETH;
-    //uint public borrowLimit ;
     uint public collateralValue ;//ביטחונות-ETH
     uint public borrowedValue ;//הלואות-
-    //uint public maxLTV ;
-   // uint public borrowRate;
     uint public poolValue;//כמה יש בבריכה-DAI
     mapping(address => uint) public lend;
     mapping(address => Borrower) public borrowers;
-    uint public ratio = 3;
-
+    uint public ratio = 0.66;//=1/1.5
+    uint ETHvalue = 1000;
+    uint wad=10**18;
+    uint fee=1.2;
+    
      constructor(address _tokenDAI,address _tokenLending,address _tokenETH){
-        tokenDAI=IERC20(tokenErc20);
-        tokenLending=IERC20(tokenErc20);
-        tokenETH=IERC20(tokenErc20);
+        tokenDAI=IERC20(_tokenDAI);
+        tokenLending=IERC20(_tokenLending);
+        tokenETH=IERC20(_tokenETH);
     }
 
      modifier isPositive(uint amount) {
-        require(amount>0,"must be better than zero");
+        require(amount>0,"must be bigger than zero");
         _; 
     }
+////////user///////
 
-    function deposite(uint amount) external isPositive(amount){
+    function deposite(uint amount) external isPositive(amount){// Deposits DAI and receives tokens
         tokenDAI.transferFrom(msg.sender,address(this),amount);
         lend[msg.sender] = amount;
         poolValue += amount;
-        //amount-?
         tokenLending.mint(msg.sender,amount);
     }
 
-    function unbond(uint amount) public isPositive(amount){
+    function unbond(uint amount) public isPositive(amount){// Pulls DAI and burns tokens
         require(lend[msg.sender].balance>=amount,"must be less amount");
         tokenDAI.transfer(msg.sender,amount);
         tokenLending.burn(msg.sender,amount);
@@ -50,21 +51,25 @@ contract Lending{
         poolValue-=amount;
     }
 
-    function addCollateral(uint amountETH) public isPositive(amountETH){
+    function addCollateral(uint amountETH) public payable isPositive(amountETH){ // Brings ETH so he can borrow
         tokenETH.transferFrom(msg.sender,address(this),amountETH);
-        collateralValue+=amount;
+        collateralValue+=amountETH;
         borrowers[msg.sender].amountCollateral+=amountETH;
     }
 
     function removeCollateral(uint amountETH) public isPositive(amountETH){
+        uint _amountBorrow = borrowers[msg.sender].amountBorrow;//666
+        uint sumForPull = (DAIforETH() - _amountBorrow)*wad/(ratio * ETHvalue)*wad;
+        require(amountETH>=sumForPull,"you can not pull, dont hava enough borrow");
         tokenETH.transfer(msg.sender,amountETH);
-        collateralValue-=amount;
+        collateralValue-=amountETH;
         borrowers[msg.sender].amountCollateral-=amountETH;
     }
 
     function borrow(uint amount) public isPositive(amount){
-        //כמה זה שווה באמת ב-DAI
+        uint dai= DAIforETH();
         require(poolValue >= amount,"not enouge money in pool");
+        require(dai>=amount+ borrowers[msg.sender].amountBorrow);
         tokenDAI.transfer(msg.sender,amount);
         borrowers[msg.sender].amountBorrow+=amount;
         borrowedValue+=amount;
@@ -74,11 +79,25 @@ contract Lending{
     function returnDAI(uint amount) public isPositive{
         require(amount<= borrowers[msg.sender].amountBorrow);
         borrowers[msg.sender].amountBorrow -= amount;
-        tokenDAI.transferFrom(msg.sender,address(this), amount);
-        poolValue += amount;
+        tokenDAI.transferFrom(msg.sender,address(this), amount*fee);
+        poolValue += amount*fee;
     }
 
-    
+
+    function DAIforETH() public view returns (uint) {
+        return ETHvalue * ratio * borrowers[msg.sender].amountCollateral;
+    }
+
+//////////owner////////////
+
+    function trove() public{
+        uint prevETH = ETHvalue;
+        // ETHvalue = קריאה לפונקציה שבודקת ערך עכשווי
+        require(ETHvalue*1.25< prevETH);
+        for (uint256 index = 0; index < array.length; index++) {
+            sale();
+        }
+    }
 }
 
 
